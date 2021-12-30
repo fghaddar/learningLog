8ab0d5728c1c244ad91ad50ad794240127363fa5
fghaddar <fghaddar@csumb.edu>
2021-12-30 15:06:37 -0800

    Styled the app using bootstrap3

--- 11_env/Scripts/learning_log/learning_log/settings.py
+++ 11_env/Scripts/learning_log/learning_log/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [                                                              
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # Third party apps
+    'bootstrap3',
+
     # My apps
     'learning_logs',
     'users',
@@ -128,3 +131,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # My settings
 LOGIN_URL = '/users/login/'
+
+# Settings for django-bootstrap3
+BOOTSTRAP3 = {
+    'include_jquery': True,
+}
